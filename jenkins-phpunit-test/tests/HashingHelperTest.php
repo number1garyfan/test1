42040a9ae5bf9ba0adca55a08867e06da938e0d8
number1garyfan <56083763+number1garyfan@users.noreply.github.com>
2020-11-09 14:14:51 +0800

    Update HashingHelperTest.php

--- jenkins-phpunit-test/tests/HashingHelperTest.php
+++ jenkins-phpunit-test/tests/HashingHelperTest.php
@@ -1,9 +1,11 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
 require_once("HelperClass/HashingHelper.php");
 
    
 
-final class HashingHelperTest extends PHPUnit_Framework_TestCase
+final class HashingHelperTest extends TestCase
 {
     //Purpose: Test for input value bigger than SHA256
     //Expected Result: if input string more than 64 characters, which is the maximum for SHA256, an empty string will returned
