6fe37140a2b373cf2435ca78987ed7eac14b8666
Boonjin123 <57652978+Boonjin123@users.noreply.github.com>
2020-11-14 20:03:20 +0800

    Update LoginXssEmailTest.java

--- src/test/java/mycompany/app/LoginXssEmailTest.java
+++ src/test/java/mycompany/app/LoginXssEmailTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.app;
+package com.mycompany.ict3103busbly;
 
 //import java.io.ByteArrayOutputStream;
 //import java.io.PrintStream;
